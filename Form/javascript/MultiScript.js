a7d4846d879d0c6d66c1eac34c15f46fed7a32bb
komaly447 <72991895+komaly447@users.noreply.github.com>
2020-10-16 22:53:45 +0530

    Update MultiScript.js

--- Form/javascript/MultiScript.js
+++ Form/javascript/MultiScript.js
@@ -1,8 +1,7 @@
-
 //jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+var current_fs, next_fs, previous_fs; 	//fieldsets
+var left, opacity, scale; 		//fieldset properties which we will animate
+var animating; 				//flag to prevent quick multi-click glitches
 
 $(".next").click(function(){
 	if(animating) return false;
@@ -14,29 +13,29 @@ $(".next").click(function(){
 	//activate next step on progressbar using the index of next_fs
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 	
-	//show the next fieldset
 	next_fs.show(); 
 	//hide the current fieldset with style
 	current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
+			
 			scale = 1 - (1 - now) * 0.2;
 			//2. bring next_fs from the right(50%)
 			left = (now * 50)+"%";
 			//3. increase opacity of next_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
+			'transform': 'scale('+scale+')',
+			'position': 'absolute'
+      		});
+		     next_fs.css({'left': left, 'opacity': opacity});
+		},
+		
+		duration: 500, 
 		complete: function(){
 			current_fs.hide();
 			animating = false;
 		}, 
+		
 		//this comes from the custom easing plugin
 		easing: 'easeInOutBack'
 	});
@@ -54,6 +53,7 @@ $(".previous").click(function(){
 	
 	//show the previous fieldset
 	previous_fs.show(); 
+	
 	//hide the current fieldset with style
 	current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
@@ -67,12 +67,12 @@ $(".previous").click(function(){
 			current_fs.css({'left': left});
 			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
 		}, 
-		duration: 800, 
+		duration: 600, 
 		complete: function(){
 			current_fs.hide();
 			animating = false;
 		}, 
-		//this comes from the custom easing plugin
+	
 		easing: 'easeInOutBack'
 	});
 });
